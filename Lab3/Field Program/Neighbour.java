14fba561b5e1793f97466d5c2e3bac5ddd20842f
yanghanxy <yanghanzzz@gmail.com>
2015-10-25 19:06:11 +0100

    Field Program (with no stress)
    We will need another version for stress (or greedy)

---
+++ Lab3/Field Program/Neighbour.java
@@ -0,0 +1,31 @@
+public class Neighbour extends Thread {
+
+    private Flag flag;
+    
+    public Neighbour(Flag flag) {
+        this.flag = flag;
+    }
+
+    public void run() {
+
+        while (true) {
+            try {
+                String name = Thread.currentThread().getName() ;
+                System.out.println("try again, my name is: "+ name);
+                Thread.sleep((int)(200*Math.random()));
+                
+                flag.set_true(name);               
+               if (!flag.query_flag(name) ) {
+                    System.out.println(name + " enter");
+                    Thread.sleep(400);
+                    System.out.println(name + " exits");
+                }
+
+                Thread.sleep((int)(200*Math.random()));
+                flag.set_false(name); 
+            }
+            catch (InterruptedException e) {};
+        }
+    }
+    
+}
