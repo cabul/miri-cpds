a752c83a0b0309edf1da6a3e271d0f64194c785f
Calvin <calvin.bulla@gmail.com>
2015-10-26 16:57:59 +0100

    Greedy mode toggle

--- Lab3/Field Program/Field.java
+++ Lab3/Field Program/Field.java
@@ -2,11 +2,20 @@ public class Field {
     
 	public static void main(String args[]) {
 
+		boolean mode;
 		Flag flag = new Flag();
 
-		Thread a = new Neighbour(flag);
+		if (args.length > 0 && args[0].equals("greedy")) {
+			System.out.println("GREEDY MODE");
+			mode = true;
+		} else {
+			System.out.println("NORMAL MODE");
+			mode = false;
+		}
+
+		Thread a = new Neighbour(flag, mode);
 		a.setName("alice");
-		Thread b = new Neighbour(flag);        
+		Thread b = new Neighbour(flag, mode);        
 		b.setName("bob");
 
 		a.start();
--- Lab3/Field Program/Neighbour.java
+++ Lab3/Field Program/Neighbour.java
@@ -1,9 +1,11 @@
 public class Neighbour extends Thread {
 
 	private Flag flag;
+	private boolean greedy;
 
-	public Neighbour(Flag flag) {
+	public Neighbour(Flag flag, boolean greedy) {
 		this.flag = flag;
+		this.greedy = greedy;
 	}
 
 	public void run() {
@@ -12,9 +14,15 @@ public class Neighbour extends Thread {
 			try {
 				String name = Thread.currentThread().getName() ;
 				System.out.println("try again, my name is: "+ name);
-				Thread.sleep((int)(200*Math.random()));
+			
+				if (this.greedy) {
+					flag.set_true(name);               
+					Thread.sleep((int)(200*Math.random()));
+				} else {
+					Thread.sleep((int)(200*Math.random()));
+					flag.set_true(name);               
+				}
 
-				flag.set_true(name);               
 				if (!flag.query_flag(name) ) {
 					System.out.println(name + " enter");
 					Thread.sleep(400);
