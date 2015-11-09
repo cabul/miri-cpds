3420ae636ee70e90a187368e1c63bae2833f2419
Calvin <calvin.bulla@gmail.com>
2015-11-09 21:06:09 +0100

    Updated code

--- Lab5/control.erl
+++ Lab5/control.erl
@@ -8,18 +8,30 @@ gen(I, N) -> [random:uniform(N) | gen(I-1, N)].
 % Worker loop
 worker(Next) ->
 	receive
+		% Receive stop signal
 		stop ->
 			Next ! stop;
-		{Ctrl, token} ->
+		% Edge case, nothing more to eat
+		{_, 0} -> worker(Next); % Do nothing
+		{Ctrl, N} ->
+			% Go to eat
 			Ctrl ! {self(), eat},
-			receive
-				% Wait for confirmation
-				% Pass token
-				ok -> Next ! {Ctrl, token},
-					  worker(Next);
-				stop -> Next ! stop
-			end
+			% Send token
+			Next ! {Ctrl, N-1},
+			% Loop
+			worker(Next)
 	end.
+
+% Note:
+% -----
+% We could also send a literal 'token'
+%    Next ! {Ctrl, token}
+% The problem with this approach is that it produces a dangling message.
+% The last worker will send an 'eat' message to control.
+% The next time we invocate the program this message will arrive!
+%
+% Another approach would be to wait for confirmation from the Control,
+% before sending the token to the next worker.
 
 % First worker
 start_worker(1) ->
@@ -28,23 +40,25 @@ start_worker(N) ->
 	Next = spawn(?MODULE, start_worker, [N-1,self()]),
 	worker(Next).
 
-% Pass along the first worker
+% Pass along the first worker.
+% The last one links with the first one.
 start_worker(1, First) ->
 	worker(First);
 start_worker(N, First) ->
 	Next = spawn(?MODULE, start_worker, [N-1,First]),
 	worker(Next).
 
-% Manage list
+% Manage list:
+% Last element
 manage([H|[]], Res) ->
 	receive {Pid, eat} ->
 		Pid ! stop,
 		[{Pid,H}|Res]
 	end;
+% General case
 manage([H|T], Res) ->
 	receive
 		{Pid, eat} ->
-			Pid ! ok,
 			manage(T,[{Pid,H}|Res])
 	end.
 
@@ -56,6 +70,8 @@ go(N,M) ->
 	L = gen(M,M),
 	io:format("~p~n", [L]),
 	First = spawn(?MODULE,  start_worker, [N]),
-	First ! {self(), token},
+	% Send first token
+	First ! {self(), M},
+	% Start control
 	manage(L, []).
 
